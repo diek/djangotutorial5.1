f770ab3e4306373388c11f60cb15fa2284d0d594
Derrick Kearney <d_kearney@bellaliant.net>
2025-03-06 13:11:47 -0400

    Add polls app, and configure urls

--- config/urls.py
+++ config/urls.py
@@ -17,8 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from debug_toolbar.toolbar import debug_toolbar_urls
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ] + debug_toolbar_urls()
