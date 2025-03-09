4a8a15a8e84b2a72c77b9738385bc66d9570e645
Derrick Kearney <d_kearney@bellaliant.net>
2025-03-08 20:51:35 -0400

    Add polish to Admin

--- polls/admin.py
+++ polls/admin.py
@@ -3,13 +3,24 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
-@admin.register(Question)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ("id", "question_text", "pub_date")
-    list_filter = ("pub_date",)
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+
+    inlines = [ChoiceInline]
+
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+
+    list_filter = ["pub_date"]
+
+    search_fields = ["question_text"]
 
 
-@admin.register(Choice)
-class ChoiceAdmin(admin.ModelAdmin):
-    list_display = ("id", "question", "choice_text", "votes")
-    list_filter = ("question",)
+admin.site.register(Question, QuestionAdmin)
